7082e66036ee57b8d107df8904b8622630195efa
maks <makser.hofman@gmail.com>
2024-07-25 21:50:55 +0200

    two scripts added (winlogs dosent work)

---
+++ stopwinupdate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+powershell -Command "Start-Process 'sc.exe' -ArgumentList 'config wuauserv start= disabled' -Verb runAs"
+powershell -Command "Start-Process 'sc.exe' -ArgumentList 'stop wuauserv' -Verb runAs"
+
+echo "Windows Update service has been disabled."
